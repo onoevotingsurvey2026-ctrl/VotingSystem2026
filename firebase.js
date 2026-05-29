2cf8202a6bef7af2b3db1d24470757bbc8435a97
onoevotingsurvey2026-ctrl <onoe.voting.survey2026@gmail.com>
2026-05-29 16:37:58 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithRedirect } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_REAL_KEY_FROM_FIREBASE",
+  apiKey: "AIzaSyBx2zElYOCWUmYzKp5p-JM6jUc7Gr5hA6o",
   authDomain: "votingsystem2026-ac105.firebaseapp.com",
   projectId: "votingsystem2026-ac105",
-  storageBucket: "votingsystem2026-ac105.appspot.com",
+  storageBucket: "votingsystem2026-ac105.firebasestorage.app",
   messagingSenderId: "640142600393",
-  appId: "1:640142600393:web:1e37148197ff33d77b0518"
+  appId: "1:640142600393:web:1e37148197ff33d77b0518",
+  measurementId: "G-09YQ30C8VK"
 };
-
-const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
-const provider = new GoogleAuthProvider();

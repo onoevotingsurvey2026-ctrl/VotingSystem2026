bf43975f1c6d5e0b622269b5f8e4d70f1998685e
onoevotingsurvey2026-ctrl <onoe.voting.survey2026@gmail.com>
2026-05-29 17:13:35 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,7 +4,15 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebas
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  const firebaseConfig = {
   apiKey: "AIzaSyBx2zElYOCWUmYzKp5p-JM6jUc7Gr5hA6o",
+  authDomain: "votingsystem2026-ac105.firebaseapp.com",
+  projectId: "votingsystem2026-ac105",
+  storageBucket: "votingsystem2026.appspot.com",
+  messagingSenderId: "640142600393",
+  appId: "1:640142600393:web:1e37148197ff33d77b0518",
+  measurementId: "G-09YQ30C8VK"
+};
   authDomain: "votingsystem2026-ac105.firebaseapp.com",
   projectId: "votingsystem2026-ac105",
   storageBucket: "votingsystem2026.appspot.com",
